b1b9c043c377310f2042c06bbd146faecad530e8
manaskumar26 <67231881+manaskumar26@users.noreply.github.com>
2020-06-21 20:23:39 +0530

    Test

---
+++ script.js
@@ -0,0 +1,75 @@
+var addList=document.getElementById('additem');
+var addul=document.getElementById('list');
+var count=0;
+var ar=[];
+
+addList.addEventListener('keypress',function(e){
+    if(!this.value)
+    {
+        return;
+    }
+    if(e.key==='Enter')
+    {
+        var item=this.value;
+        var elem=document.createElement("li");
+        elem.innerHTML=item+"<span class='align'><button class='btn'>X</button></span>";
+        addul.appendChild(elem);
+        this.value="";
+        count++;
+        elem.addEventListener('click',del);
+        
+
+    }    
+    if(count===0)
+    {
+        return;
+    }
+    document.getElementById('tasks').innerHTML=count;
+});
+
+
+function del()
+{
+    var complete=this.parentNode.removeChild(this);
+    count--;
+    var str=complete.innerText;
+    ar.push(str.substring(0,str.length-1));
+    
+    document.getElementById('tasks').innerHTML=count;
+    if(count==0)
+    {
+        document.getElementById('tasks').innerHTML='No';
+
+    }
+
+}
+var target=document.getElementById('completed');
+target.addEventListener('click',function(){
+    
+
+    var completelist=document.getElementById('listcomplete');
+    var final=document.getElementById('finale');
+    final.style.display='block';
+
+    
+    for(var i=0;i<ar.length;i++)
+    {
+        var ele=document.createElement('li');
+        ele.innerText=ar[i];
+        completelist.appendChild(ele);
+
+    }
+   
+
+});
+var clear=document.getElementById('clr');
+clear.addEventListener('click',function(event){
+    event.preventDefault();
+    document.getElementById("listcomplete").innerHTML = "";
+
+    var final=document.getElementById('finale');
+    final.style.display='none';
+
+
+    
+});
